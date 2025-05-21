7d31d6404bd326c69b2024bdd360a57273bfeaf7
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-21 14:56:29 -0300

    feat: eval calculate result

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -44,6 +44,32 @@ class CalController {
     return ["+", "-", "*", "%", "/"].indexOf(value) > -1;
   }
 
+  pushOperation(value) {
+
+    this._operation.push(value)
+
+    if(this._operation.length > 3) {
+        let last = this._operation.pop();
+
+        this.calc();
+
+        console.log(this._operation)
+    }
+  }
+
+  calc() {
+
+    let last = this._operation.pop();
+
+    let result = eval(this._operation.join(""))
+
+    this._operation = [result, last];
+  }
+
+  setLastNumberToDisplay() {
+    
+  }
+
   addOperation(value) {
     console.log("A", isNaN(this.getLastOperation()));
 
@@ -53,12 +79,18 @@ class CalController {
       } else if (isNaN(value)) {
         console.log(value);
       } else {
-        this._operation.push(value);
+        this.pushOperation(value);
       }
     } else {
-      let newValue = this.getLastOperation().toString() + value.toString();
+      if (this.isOperator(value)) {
+        this.pushOperation(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
 
-      this.setLastOperation(parseInt(newValue));
+        this.setLastOperation(parseInt(newValue));
+
+        this.setLastNumberToDisplay();
+      }
     }
 
     console.log(this._operation);
