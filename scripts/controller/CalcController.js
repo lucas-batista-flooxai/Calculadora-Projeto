7a7131d195ab89fbf7a07a2661e5abdeb9f3fb29
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-21 12:06:00 -0300

    feat: manipulating DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,18 @@
 class CalController {
   constructor() {
     this._displayCalc = "0";
-    this._dataAtual;
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    let displayCalcEl = document.querySelector("#display");
+    let dateEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+
+    displayCalcEl.innerHTML = "4567";
+    dateEl.innerHTML = "20 de Maio";
+    timeEl.innerHTML = "24 horas";
   }
 
   get displayCalc() {
@@ -12,11 +23,11 @@ class CalController {
     this._displayCalc = valor;
   }
 
-  get dataAtual() {
-    return this._dataAtual;
+  get currentDate() {
+    return this._currentDate;
   }
 
-  set dataAtual(valor) {
-    this._dataAtual = valor;
+  set currentDate(valor) {
+    this._currentDate = valor;
   }
 }
