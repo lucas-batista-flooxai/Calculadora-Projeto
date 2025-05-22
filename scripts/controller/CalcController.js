4f065102a3caa43c4c9a95a361992f4cf375ef37
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-22 12:37:03 -0300

    fix: fix dot bug

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -144,7 +144,7 @@ class CalController {
       } else {
         let newValue = this.getLastOperation().toString() + value.toString();
 
-        this.setLastOperation(parseFloat(newValue));
+        this.setLastOperation(newValue);
 
         this.setLastNumberToDisplay();
       }
@@ -158,6 +158,8 @@ class CalController {
   addDot() {
 
     let lastOperation = this.getLastOperation();
+
+    if (typeof lastOperation === 'string' && lastOperation.lastOperation.split('').indexOf('.') > -1) return
 
     if(this.isOperator(lastOperation) || !lastOperation) {
       this.pushOperation('0.')
